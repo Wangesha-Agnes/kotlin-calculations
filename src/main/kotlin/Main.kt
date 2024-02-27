116737123bbe0af5e9451fe4db76cb9052d303d5
Student <wangeshaagnes@gmail.com>
2024-02-27 07:28:07 +0300

    salary calculation

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+fun main() {
+    salary (6,1500)
+    salary (5,1500)
+    salary (2,1500)
+    salary (4,1500)
+    salary (7,1500)
+}
+
+    fun salary(num1:Int, num2:Int):Int{
+        var total = num1 * num2
+        println (total)
+        return total
+    }
